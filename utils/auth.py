38e1436bf3cac5c023db0ed9f1742e494b2ea64a
JuanCManchegoH <juan.manchegoh@mainhightech.com>
2023-09-08 00:46:30 -0500

    [UPDATE] fixing token time to expire

--- utils/auth.py
+++ utils/auth.py
@@ -10,7 +10,7 @@ db = db_client["harmony"]
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 120
+ACCESS_TOKEN_EXPIRE_WEEKS =  2
 
 def verifyPassword(plainPassword, hashedPassword) -> bool:
     return pwd_context.verify(plainPassword, hashedPassword)
@@ -23,7 +23,7 @@ def createAccessToken(data: dict, expires_delta: timedelta = None) -> str:
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(weeks=ACCESS_TOKEN_EXPIRE_WEEKS)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
